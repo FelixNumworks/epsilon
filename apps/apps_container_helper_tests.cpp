adef9e5beec73aaa9e3378b729cc7b2e59ebc478
Félix Richart <felix.richart@numworks.com>
2023-06-09 14:14:03 +0200

    t

--- apps/apps_container_helper_tests.cpp
+++ apps/apps_container_helper_tests.cpp
@@ -1,6 +1,8 @@
 #include <apps/apps_container_helper.h>
 
-Shared::GlobalContext * AppsContainerHelper::sharedAppsContainerGlobalContext() {
+// Modification
+
+Shared::GlobalContext* AppsContainerHelper::sharedAppsContainerGlobalContext() {
   // Define a single shared GlobalContext
   static Shared::GlobalContext s_globalContext;
   return &s_globalContext;
